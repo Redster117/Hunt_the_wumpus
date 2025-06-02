04286aaa50b12ae55b514bdf9dd179f26896a63d
Jarred Joseph Cuschieri <jarred.cuschieri@education.nsw.gov.au>
2025-06-02 01:06:45 +0000

    Continued with the code

--- cave.py
+++ cave.py
@@ -1,20 +1,37 @@
-class cave:
+"""This module defines a class Cave."""
+class Cave:
+    """Defines the self name."""
     def __init__(self, cave_name):
         self.name = cave_name
         self.description = None
-        self.linked_caves = 
-    
+        self.linked_caves = {}
+
     def set_name(self, cave_name):
+        """Defines the set name"""
         self.name = cave_name
 
     def get_name(self):
+        """Defines the get name"""
         return self.name
 
     def set_description(self, cave_description):
+        """Defines the set description"""
         self.description = cave_description
 
     def get_description(self):
+        """Defines the get description"""
         return self.description
 
     def describe(self):
+        """Prints the description of the cave."""
         print(self.description)
+
+    def link_caves(self, cave_to_link, direction):
+        """Links this cave to another cave in a specified direction."""
+        self.linked_caves[direction] = cave_to_link
+        print(self.name + " linked caves:" + repr(self.linked_caves))
+
+    def get_details(self):
+        """Return the details of the cave."""
+        for direction, cave in self.linked_caves.items():
+            print("The " + cave.get_name() + " is " + direction)
--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+"""Info about the cave system."""
 from cave import Cave
 
 cavern = Cave("Cavern")
 cavern.set_description("A dark and damp cavern with stalactites hanging from the ceiling.")
 grotto = Cave("Grotto")
+grotto.set_description("A small grotto with a serene pool of water.")
 dungeon = Cave("Dungeon")
+dungeon.set_description("A foreboding dungeon with stone walls.")
+
+cavern.link_caves(dungeon, "South")
+dungeon.link_caves(cavern, "North")
+dungeon.link_caves(grotto, "West")
+grotto.link_caves(dungeon, "East")
+
+cavern.get_details()
+dungeon.get_details()
+grotto.get_details()
+
 cavern.describe()
+grotto.describe()
+dungeon.describe()
